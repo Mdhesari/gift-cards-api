1a986e5e0e44011542aafe3e35281997eb3a7943
Mohamad Hesari <mdhesari99@gmail.com>
2024-01-31 16:58:09 +0330

    feat(entity): git card

---
+++ app/Models/GiftCard.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GiftCard extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'remaining_balance',
+        'max_users',
+        'users_used',
+        'quantity',
+    ];
+}
